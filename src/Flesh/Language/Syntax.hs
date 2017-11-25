c1aaed9fe503e27e848138c17a67cca661dddeb1
WATANABE Yuki <magicant@wonderwand.net>
2017-11-25 18:25:09 +0900

    Remove explicit position from assignments in simple commands
    Every assignment has a non-empty name from which the position can be
    identified.

--- src/Flesh/Language/Syntax.hs
+++ src/Flesh/Language/Syntax.hs
@@ -293,7 +293,7 @@ instance Show CompoundCommand where
 -- | Element of pipelines.
 data Command =
   -- | Simple command.
-  SimpleCommand [Token] [Positioned Assignment] [Redirection]
+  SimpleCommand [Token] [Assignment] [Redirection]
   -- | Compound commands
   | CompoundCommand (Positioned CompoundCommand) [Redirection]
   -- | Function definition.
@@ -303,13 +303,11 @@ data Command =
 instance Show Command where
   showsPrec _ (SimpleCommand [] [] []) = id
   showsPrec _ (SimpleCommand ts [] []) = showList ts
-  showsPrec _ (SimpleCommand [] as []) = showList as'
-    where as' = snd <$> as
+  showsPrec _ (SimpleCommand [] as []) = showList as
   showsPrec _ (SimpleCommand [] [] rs) = showList rs
   showsPrec _ (SimpleCommand ts [] rs) = showList ts . showSpace . showList rs
   showsPrec n (SimpleCommand ts as rs) =
-    showList as' . showSpace . showsPrec n (SimpleCommand ts [] rs)
-    where as' = snd <$> as
+    showList as . showSpace . showsPrec n (SimpleCommand ts [] rs)
   showsPrec n (CompoundCommand (_, cc) []) = showsPrec n cc
   showsPrec n (CompoundCommand (_, cc) rs) =
     showsPrec n cc . showSpace . showList rs
