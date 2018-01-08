e90daed5f4c4f426b09fdd48b10b68cadd832c32
WATANABE Yuki <magicant@wonderwand.net>
2018-01-08 16:07:43 +0900

    Split ClassSpec from ErrorSpec

--- hspec-src/Flesh/Language/Parser/ClassSpec.hs
+++ hspec-src/Flesh/Language/Parser/ClassSpec.hs
@@ -19,18 +19,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Flesh.Language.Parser.ErrorSpec (spec) where
+module Flesh.Language.Parser.ClassSpec (spec) where
 
-import Control.Monad.Except (MonadError, ExceptT, runExceptT)
+import Control.Applicative ((<|>))
+import Control.Monad.Except (MonadError, ExceptT, mapExceptT, runExceptT)
 import Control.Monad.Identity (Identity, runIdentity)
+import Control.Monad.State.Strict (runState)
 import Flesh.Language.Parser.Class
 import Flesh.Language.Parser.Error
 import Flesh.Language.Parser.Input
 import Flesh.Source.Position
 import Test.Hspec (Spec, describe)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (
-  Arbitrary, Gen, arbitrary, elements, oneof, (===), (==>))
+import Test.QuickCheck (Arbitrary, Gen, arbitrary, elements, oneof, (===))
 
 instance Arbitrary Reason where
   arbitrary = oneof [
@@ -65,60 +66,49 @@ instance (MonadError Failure m, Arbitrary a) => Arbitrary (ParserT m a) where
             e <- arbitrary :: Gen Error
             return $ throwError (s, e)
 
+instance Arbitrary a => Arbitrary (PositionedList a) where
+  arbitrary = do
+    s <- arbitrary
+    xs <- arbitrary
+    return $ spread (dummyPosition s) xs
+
 type AE = ParserT (RecordT (ExceptT Failure Identity))
+type AES = ParserT (RecordT (ExceptT Failure (PositionedStringT Identity)))
 
-runAE :: AE a -> Either Failure a
-runAE = runIdentity . runExceptT . evalRecordT . runParserT
+run :: AES a -> PositionedString -> (Either Failure a, PositionedString)
+run = runState . runPositionedStringT . runExceptT . evalRecordT . runParserT
 
-isUnknownReason :: AE a -> Bool
-isUnknownReason a =
-  case runAE a of
-    Left (_, Error UnknownReason _) -> True
-    _                               -> False
-
-isHardError :: AE a -> Bool
-isHardError a =
-  case runAE a of
-    Left (Hard, _) -> True
-    _              -> False
-
-isSoftError :: AE a -> Bool
-isSoftError a =
-  case runAE a of
-    Left (Soft, _) -> True
-    _              -> False
+aesFromAE :: AE a -> AES a
+aesFromAE = mapParserT $ mapRecordT $ mapExceptT $ return . runIdentity
 
 spec :: Spec
 spec = do
-  describe "setReason" $ do
-    prop "replaces UnknownReason" $ \s e ->
-      let Error r p = e
-          a         = throwError (s, e) :: AE Int
-          a'        = throwError (s, Error UnknownReason p)
-       in setReason r a' === a
+  describe "notFollowedBy" $ do
+    prop "succeeds if argument fails" $ \e i ->
+      let f = failureOfError e
+       in run (notFollowedBy f) i === run (return ()) i
 
-    prop "retains known reason" $ \r a ->
-      not (isUnknownReason (a :: AE Int)) ==>
-        setReason r a === a
+    prop "fails if argument succeeds" $ \v i ->
+      let _ = v :: Int
+       in run (notFollowedBy (return v)) i === run failure i
 
-  describe "try" $ do
-    prop "converts hard errors to soft" $ \e ->
-      let h = throwError (Hard, e) :: AE Int
-          s = throwError (Soft, e) :: AE Int
-       in try h === s
-
-    prop "retains successes and soft errors intact" $ \a ->
+  describe "Alternative (ParserT m) (<|>)" $ do
+    prop "returns hard errors intact" $ \e a i ->
       let _ = a :: AE Int
-       in not (isHardError a) ==> try a === a
+          a' = aesFromAE a
+          f  = require $ failureOfError e
+       in run (f <|> a') i === run f i
 
-  describe "require" $ do
-    prop "converts soft errors to hard" $ \e ->
-      let h = throwError (Hard, e) :: AE Int
-          s = throwError (Soft, e) :: AE Int
-       in require s === h
-
-    prop "retains successes and hard errors intact" $ \a ->
+    prop "returns success intact" $ \v a i ->
       let _ = a :: AE Int
-       in not (isSoftError a) ==> require a === a
+          a' = aesFromAE a
+          s  = return v
+       in run (s <|> a') i === run s i
+
+    prop "recovers soft errors" $ \e a i ->
+      let _ = a :: AE Int
+          a' = aesFromAE a
+          f  = failureOfError e
+       in run (f <|> a') i === run a' i
 
 -- vim: set et sw=2 sts=2 tw=78:
